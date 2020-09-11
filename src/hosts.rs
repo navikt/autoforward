4eb6fc859d8aa746f2b80c4b2dd9b311e3f5086c
Christian C <christian.chavez@nav.no>
2020-09-11 16:02:41 +0200

    refactor: Move tests into their own module
    Collect them into "one place" at end of file.

--- src/hosts.rs
+++ src/hosts.rs
@@ -1,6 +1,5 @@
 use std::io;
 use std::path::Path;
-use std::str;
 use std::io::Write;
 use std::fs::OpenOptions;
 
@@ -16,22 +15,6 @@ const LINE_SEPARATOR: &'static [u8] = b"\n";
 #[cfg(windows)]
 const LINE_SEPARATOR: &'static [u8] = b"\r\n";
 
-#[test]
-fn update_hosts_does_not_replace() {
-    let hosts = vec!["reddit.com".to_owned()];
-    let mut target_hosts = tempfile::NamedTempFile::new().unwrap();
-    std::fs::copy(Path::new("testdata/hosts"), &target_hosts.path()).unwrap();
-    update_hosts_file(&target_hosts.path(), &hosts).unwrap();
-    let original = std::fs::read_to_string(&target_hosts).unwrap();
-
-    update_hosts_file(&target_hosts.path(), &hosts).unwrap();
-    update_hosts_file(&target_hosts.path(), &hosts).unwrap();
-
-    let updated = std::fs::read_to_string(&target_hosts).unwrap();
-
-    assert_eq!(original, updated);
-}
-
 pub fn update_hosts_file(path: &Path, hosts: &Vec<String>) -> Result<(), io::Error> {
     let input_bytes = std::fs::read(path)?;
 
@@ -42,44 +25,6 @@ pub fn update_hosts_file(path: &Path, hosts: &Vec<String>) -> Result<(), io::Err
     let result = insert_or_replace_entries(&input_bytes, &generate_host_entries(hosts));
     output.write_all(&result)?;
     Ok(())
-}
-
-#[test]
-fn test_replace_entries() {
-    let input = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
-### START AUTOFORWARD
-127.0.0.1 speil.nais.preprod.local
-### END AUTOFORWARD
-127.0.0.1 localhost
-"#.as_bytes();
-    let hosts = generate_host_entries(&vec!["new.nais.preprod.local".to_owned()]);
-    let expected = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
-### START AUTOFORWARD
-127.0.0.1 new.nais.preprod.local
-### END AUTOFORWARD
-127.0.0.1 localhost
-"#;
-
-    assert_eq!(str::from_utf8(insert_or_replace_entries(input, &hosts).as_slice()).unwrap(), expected);
-}
-
-#[test]
-fn append_entries() {
-    let input = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
-127.0.0.1 localhost
-"#.as_bytes();
-
-    let hosts = generate_host_entries(&vec!["new.nais.preprod.local".to_owned()]);
-
-    let expected = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
-127.0.0.1 localhost
-
-### START AUTOFORWARD
-127.0.0.1 new.nais.preprod.local
-### END AUTOFORWARD
-"#;
-    assert_eq!(str::from_utf8(insert_or_replace_entries(input, &hosts).as_slice()).unwrap(), expected);
-
 }
 
 fn insert_or_replace_entries(input: &'_ [u8], replacement: &[u8]) -> Vec<u8> {
@@ -125,4 +70,66 @@ fn generate_host_entries(hosts: &Vec<String>) -> Vec<u8> {
     }
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str;
+    use super::*;
+
+    #[test]
+    fn test_replace_entries() {
+        let input = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
+### START AUTOFORWARD
+127.0.0.1 speil.nais.preprod.local
+### END AUTOFORWARD
+127.0.0.1 localhost
+"#.as_bytes();
+        let hosts = generate_host_entries(&vec!["new.nais.preprod.local".to_owned()]);
+        let expected = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
+### START AUTOFORWARD
+127.0.0.1 new.nais.preprod.local
+### END AUTOFORWARD
+127.0.0.1 localhost
+"#;
+
+        assert_eq!(str::from_utf8(insert_or_replace_entries(input, &hosts).as_slice()).unwrap(), expected);
+    }
+
+
+    #[test]
+    fn append_entries() {
+        let input = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
+127.0.0.1 localhost
+"#.as_bytes();
+
+        let hosts = generate_host_entries(&vec!["new.nais.preprod.local".to_owned()]);
+
+        let expected = r#"# This is a commentæøå¡™£¢∞∞§¶•ª¶§∞¢£🦀
+127.0.0.1 localhost
+
+### START AUTOFORWARD
+127.0.0.1 new.nais.preprod.local
+### END AUTOFORWARD
+"#;
+        assert_eq!(str::from_utf8(insert_or_replace_entries(input, &hosts).as_slice()).unwrap(), expected);
+
+    }
+
+
+    #[test]
+    fn update_hosts_does_not_replace() {
+        let hosts = vec!["reddit.com".to_owned()];
+        let mut target_hosts = tempfile::NamedTempFile::new().unwrap();
+        std::fs::copy(Path::new("testdata/hosts"), &target_hosts.path()).unwrap();
+        update_hosts_file(&target_hosts.path(), &hosts).unwrap();
+        let original = std::fs::read_to_string(&target_hosts).unwrap();
+
+        update_hosts_file(&target_hosts.path(), &hosts).unwrap();
+        update_hosts_file(&target_hosts.path(), &hosts).unwrap();
+
+        let updated = std::fs::read_to_string(&target_hosts).unwrap();
+
+        assert_eq!(original, updated);
+    }
 }
